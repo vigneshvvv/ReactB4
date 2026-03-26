9b7a4b4ea7a7e1df2abf8a574ed9a89bb4c63bde
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-03-26 09:58:28 +0530

    Add password confirmation and validation logic

--- myapp/src/contextApi/InputComponent.jsx
+++ myapp/src/contextApi/InputComponent.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import LoginContext from "./loginContext";
 
@@ -17,6 +17,36 @@ function InputComponent(){
 
     const navigate = useNavigate();
     const {setLoginUser} = useContext(LoginContext);
+
+    const[passwordOne, setPasswordOne] = useState("");
+    const[passwordTwo, setPasswordTwo] = useState("");
+    const [match, setMatch] = useState(false);
+    const[buttonState, setButtonState] = useState(true)
+
+    function handlePasswordOne(event){
+      setPasswordOne(event.target.value);
+      console.log(passwordOne);
+    }
+
+    const handlePasswordTwo = (event) => {
+      setPasswordTwo(event.target.value);
+      console.log(passwordTwo);      
+    }
+
+    const handleRadioButton = (event) => {
+      console.log(event)
+      if(passwordOne === passwordTwo){
+        console.log("matching")
+        setMatch(false);
+        setButtonState(false)
+        console.log(match)
+
+      }else{
+        setMatch(true)
+        console.log("not matching")
+      }
+
+    }
 
     const submitHandle = (event) => {
         event.preventDefault();
@@ -46,20 +76,28 @@ function InputComponent(){
   <div class="row mb-3">
     <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3" name="password"/>
+      <input type="password" value={passwordOne} onChange={handlePasswordOne} class="form-control" id="inputPassword3" name="password"/>
+    </div>
+  </div>
+  <div class="row mb-3">
+    <label for="inputPassword4" class="col-sm-2 col-form-label">Re-EnterPassword</label>
+    <div class="col-sm-10">
+      <input type="password" value={passwordTwo} onChange={handlePasswordTwo} class="form-control" id="inputPassword4" name="password"/>
     </div>
   </div>
   <fieldset class="row mb-3">
     <legend class="col-form-label col-sm-2 pt-0"></legend>
     <div class="col-sm-10">
       <div class="form-check">
-        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked/>        <label class="form-check-label" for="gridRadios1">
+        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" onChange={handleRadioButton} value="option1"/>        
+        <label class="form-check-label" for="gridRadios1">
           Remember Me
         </label>
       </div>
     </div>
+    {match && <p>The passwords are not matching ! please enter again</p>}
   </fieldset>
-  <button type="submit" class="btn btn-primary">Sign in</button>
+  <button disabled={buttonState} type="submit"  class="btn btn-primary">Sign in</button>
 </form>
         </>
     );
